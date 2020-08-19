86b4276b1b139b317b19f1f3899df69bf5b19d44
Luis Israel Sanchez Escbar <isanchez@trigarante.com>
2020-08-18 22:49:00 -0500

    Componentes, Rutas y diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './Modules/home/home.component';
+import {Err404Component} from './complements/err404/err404.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: Err404Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Err404Component } from './complements/err404/err404.component';
+import { HomeComponent } from './Modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Err404Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
