8e11787446e21eac46a3770602852d7b45f08773
Luis Israel Sanchez Escbar <isanchez@trigarante.com>
2020-08-20 17:19:01 -0500

    Loader Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './Modules/home/home.component';
 import {Err404Component} from './complements/err404/err404.component';
+import {LoaderComponent} from './complements/loader/loader.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'loader', component: LoaderComponent},
   {path: '**', component: Err404Component},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Err404Component } from './complements/err404/err404.component';
 import { HomeComponent } from './Modules/home/home.component';
+import { LoaderComponent } from './complements/loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Err404Component,
-    HomeComponent
+    HomeComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
