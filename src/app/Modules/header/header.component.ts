e63747fad1d4f60d99d5de2e5f5bcc81ba4b0491
Israel Sanchez <isanchez@trigaranter.com>
2021-02-12 00:42:45 -0600

    Cambios en header y global styles

--- src/app/Modules/header/header.component.ts
+++ src/app/Modules/header/header.component.ts
@@ -11,21 +11,5 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.menuAction();
-  }
-  menuAction(): void {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-    const links = document.querySelectorAll('.nav-links li');
-
-    hamburger.addEventListener('click', () => {
-      // ANIMATED LINKS
-      navLinks.classList.toggle('open');
-      links.forEach(link => {
-        link.classList.toggle('fade');
-      });
-      // ANIMATION MENU
-      hamburger.classList.toggle('toggle');
-    });
   }
 }
